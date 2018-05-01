6bcc08dd44c8907f1f860d179bc254a32a82c76f
rookiepeng <rookie.dev@gmail.com>
2018-04-30 21:29:17 -0500

    Display Ad banner after the ad is loaded

--- app/src/main/java/com/rookiedev/microwavetools/fragments/AdFragment.java
+++ app/src/main/java/com/rookiedev/microwavetools/fragments/AdFragment.java
@@ -7,11 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.rookiedev.microwavetools.R;
 
 public class AdFragment extends Fragment {
+    private AdView banner;
     public AdFragment() {
     }
 
@@ -19,7 +21,36 @@ public class AdFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View adView = inflater.inflate(R.layout.fragment_ad_banner, container, false);
 
-        AdView banner = (AdView) adView.findViewById(R.id.adView);
+        banner = (AdView) adView.findViewById(R.id.adView);
+        banner.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                banner.setVisibility(AdView.VISIBLE);
+                // Code to be executed when an ad finishes loading.
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when when the user is about to return
+                // to the app after tapping on an ad.
+            }
+        });
         AdRequest adRequest = new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR) // All emulators
                 .addTestDevice("2D302053F176402A9C59EB67575E6977")
                 .build();
