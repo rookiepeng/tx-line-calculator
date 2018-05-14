3b41d6b5ea36079be46703dbfe8c6b91b91b9e48
rookiepeng <rookie.dev@gmail.com>
2018-05-13 23:00:07 -0500

    add a restore icon

--- mobile/src/main/java/com/rookiedev/microwavetools/MainActivity.java
+++ mobile/src/main/java/com/rookiedev/microwavetools/MainActivity.java
@@ -6,7 +6,9 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
@@ -68,6 +70,15 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         navigationView = findViewById(R.id.nav_view);
         assert navigationView != null;
